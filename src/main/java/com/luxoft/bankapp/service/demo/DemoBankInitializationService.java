9a6c77165f126d85c1242d41de1f5090adc3c5a1
oporanuioan <oporanuioan1107@gmail.com>
2023-11-21 19:58:18 +0200

    ex4-t2

--- src/main/java/com/luxoft/bankapp/service/demo/DemoBankInitializationService.java
+++ src/main/java/com/luxoft/bankapp/service/demo/DemoBankInitializationService.java
@@ -1,11 +1,22 @@
 package com.luxoft.bankapp.service.demo;
 
+import com.luxoft.bankapp.service.feed.BankFeedServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+import java.io.File;
+
+@Profile("dev")
 @Service
 public class DemoBankInitializationService implements BankInitializationService {
-    @Override
+    @Autowired
+    BankFeedServiceImpl feedService;
+
+    @PostConstruct
     public void createClientsForDemo() {
 
+        feedService.loadFeed(new File("demo.feed"));
     }
 }
